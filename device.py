366b8c2508e13dea35b0ea3aaffa8b4f56673fed
= <=>
2022-01-28 22:50:50 +0000

    added change handler

--- device.py
+++ device.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import json
+import dbus
 
 AppDir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(1, os.path.join(AppDir, 'ext', 'velib-python'))
@@ -18,9 +19,12 @@ class MQTTDevice(object):
 			else dbus.bus.BusConnection(dbus_address)
         self._status = device_status
         self._clientId = device_status["clientId"]
-        self._settings = SettingsDevice(bus=self._dbus_conn)
+        self._settings = SettingsDevice(bus=self._dbus_conn, supportedSettings={}, eventCallback=_handle_changed_settings)
         self._services = {}
         self._register_device_services()
+
+    def _handle_changed_setting(setting, oldvalue, newvalue):
+        print("setting changed, setting: {}, old: {}, new: {}".format(setting, oldvalue, newvalue))
 
     def _serviceId(self, service):
         return 'mqtt_{}_{}'.format(self._clientId, service)
