12ad01a2140e954af4c58cc05e307e0d863fefbe
= <=>
2022-02-04 15:35:16 +0000

    Added dbus paths

--- device.py
+++ device.py
@@ -29,16 +29,37 @@ class MQTTDevice(object):
     def _serviceId(self, service):
         return 'mqtt_{}_{}'.format(self._clientId, service)
     
+    def _servicePath(self, service):
+        return 'com.victronenergy.{}.mqtt{}'.format(service, self._clientId)
 
     def _register_device_services(self):
         for service in self._status["services"]:
             logging.info("Registering service {} for client {}", service, self._clientId)
+
             path = "/Settings/Devices/{}/ClassAndVrmInstance".format(self._serviceId(service))
             requested_id = "{}:1".format(service)
             res = self._settings.addSetting(path, requested_id, "", "")
             s, device_instance = res.get_value().split(':')
+
+            dbus_service = VeDbusService(self._servicePath(service), bus=_self.dbus_conn)
+            # Add objects required by ve-api
+            dbus_service.add_path('/Management/ProcessName', 'dbus-mqtt-devices')
+            dbus_service.add_path('/Management/ProcessVersion', VERSION)
+            dbus_service.add_path('/Management/Connection', 'mqtt{}'.format(self._clientId))
+            dbus_service.add_path('/DeviceInstance', device_instance)
+            dbus_service.add_path('/ProductId', 0xFFFF) # ???
+            dbus_service.add_path('/ProductName', "{} Sensor via MQTT".format(service))
+            dbus_service.add_path('/FirmwareVersion', None)
+            dbus_service.add_path('/Connected', 1)
+
+            dbus_service.add_path('/CustomName', value='FreakEnt Temp Sensor', writeable=True)
+		    dbus_service.add_path('/TemperatureType', value=2, writeable=True)
+		    dbus_service.add_path('/Temperature', value=5, description="Cabin temperature", writeable=True)
+		    dbus_service.add_path('/Humidity', value=59.56, description="Cabin humidity", writeable=True)
+		    #dbus_service.add_path('/Pressure', value=None, description="Cabin pressure", writeable=True)
+            
             logging.info("Registered Service under %s (%s)", res.get_value(), device_instance)
-            self._services[service] = device_instance
+            self._services[service] = {"deviceInstance": device_instance, "dbusService": dbus_service}
 
     def device_instance(self):
         return self._services
