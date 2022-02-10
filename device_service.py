029bc9cfda74972da8d49643574c7f140ab83a63
= <=>
2022-02-10 22:20:23 +0000

    Added service name to settings

--- device_service.py
+++ device_service.py
@@ -51,8 +51,8 @@ class MQTTDeviceService(object):
 
     def _set_up_local_settings(self):
         local_settings = {
-            'CustomName': ["/Settings/MqttDevices/{}/CustomName".format(self.serviceId(self.service)), 'My Temp Sensor', 0, 0],
-            'TemperatureType': ["/Settings/MqttDevices/{}/TemperatureType".format(self.serviceId(self.service)), 2, 0, 2],
+            'CustomName': ["/Settings/MqttDevices/{}/CustomName".format(self.serviceName()), 'My Temp Sensor', 0, 0],
+            'TemperatureType': ["/Settings/MqttDevices/{}/TemperatureType".format(self.serviceName()), 2, 0, 2],
         }
         self._settings = SettingsDevice(bus=self._dbus_conn, supportedSettings=local_settings, eventCallback=self._handle_changed_setting)
 
