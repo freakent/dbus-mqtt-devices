bce2dc33f731138900baa6ecbb50412d9acc4c96
= <=>
2022-02-18 21:09:18 +0000

    Improve install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-BASE=/data/drivers/dbus-mqtt-devices
+#BASE=/data/drivers/dbus-mqtt-devices
+BASE=$(dirname $(dirname $(realpath "$0")))
 cd $BASE
+echo "Ensure Python's Pip is installed..."
 python -m ensurepip
+echo "Pip install module dependencies..."
 python -m pip install -r requirements.txt
+echo "Set up device service to autorun on restart"
 chmod -R a+rwx $BASE/bin/service
+rm -fq /service/dbus-mqtt-devices
 ln -s $BASE/bin/service /service/dbus-mqtt-devices
 echo "ln -s $BASE/bin/service /service/dbus-mqtt-devices" >> /data/rc.local
+echo "Install complete"
