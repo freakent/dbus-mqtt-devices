0f47c7e7e84e47ffbfc291486c79784f6c0c4af7
Martin Jarvis <martin@freakent.co.uk>
2022-12-24 08:25:10 +0000

    Improve setup script
    Improve messages formatting
    Silence errors from grep if /data/rc.local doesn't already exist

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,25 +2,25 @@
 #BASE=/data/drivers/dbus-mqtt-devices
 BASE=$(dirname $(dirname $(realpath "$0")))
 
-echo "Setup dbus-mqtt-devices in $BASE started"
+echo "dbus-mqtt-devices: Setup in $BASE started"
 cd $BASE
 
-echo "Ensure Python's Pip is installed"
+echo "dbus-mqtt-devices: Checking to see if Python's Pip is installed"
 python -m pip --version
 piperr=$?
 if [ "$piperr" -ne 0 ]; then
     opkg update && opkg install python3-pip
 fi
 
-echo "Pip install module dependencies"
+echo "dbus-mqtt-devices: Pip install module dependencies"
 python -m pip install -r requirements.txt
 
-echo "Set up Victron module libraries"
+echo "dbus-mqtt-devices: Set up Victron module libraries"
 rm -fr $BASE/ext/dbus-mqtt $BASE/ext/velib_python
 ln -s /opt/victronenergy/dbus-mqtt $BASE/ext
 ln -s /opt/victronenergy/dbus-digitalinputs/ext/velib_python $BASE/ext
 
-echo "Set up device service to autorun on restart"
+echo "dbus-mqtt-devices: Set up device service to autorun on restart"
 chmod +x $BASE/dbus_mqtt_devices.py
 # Use awk to inject correct BASE path into the run script
 awk -v base=$BASE '{gsub(/\$\{BASE\}/,base);}1' $BASE/bin/service/run.tmpl >$BASE/bin/service/run
@@ -28,9 +28,10 @@ chmod -R a+rwx $BASE/bin/service
 rm -f /service/dbus-mqtt-devices
 ln -s $BASE/bin/service /service/dbus-mqtt-devices
 
+echo "dbus-mqtt-devices: Adding device service to /data/rc.local"
 CMD="ln -s $BASE/bin/service /service/dbus-mqtt-devices"
-if ! grep -q "$CMD" /data/rc.local; then
+if ! grep -s -q "$CMD" /data/rc.local; then
     echo "$CMD" >> /data/rc.local
 fi
 chmod +x /data/rc.local
-echo "Setup dbus-mqtt-devices complete"
+echo "dbus-mqtt-devices: Setup complete"
