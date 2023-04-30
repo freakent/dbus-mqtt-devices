fa53da8a8f605df06941e62d95ca113b2dc9d67d
= <=>
2023-05-01 00:07:10 +0100

    Added checks to not reinstall PyYAML if it already exists

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,15 +12,25 @@ if [ "$piperr" -ne 0 ]; then
     opkg update && opkg install python3-modules python3-pip
 fi
 
-memory=(`free | grep Mem:`)
-if [ ${memory[1]} -le 244400 ]; then
-  echo "dbus-mqtt-devices: CCGX device detected, basic install of PyYaml required"
-  wget -qO- https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz | tar xvz -C $BASE/ext
-  python $BASE/ext/PyYAML-6.0/setup.py --without-libyaml install
-else
-  echo "dbus-mqtt-devices: Pip install module dependencies"
-  python -m pip install -r requirements.txt
-fi
+# check to see if we have a YAML parser (PyYaml)
+echo "import yaml" | python                                           
+pyyamlerr=$?                                                          
+if [ "$pyyamlerr" -ne 0 ]; then  # We don't have a yaml parser so we'll instal PyYaml                                     
+  memory=(`free | grep Mem:`)                                         
+  if [ ${memory[1]} -le 244400 ]; then # Check to see if we have small RAM like CCGX,                                
+    echo "dbus-mqtt-devices: CCGX device detected, basic install of PyYaml required"
+    wget -qO- https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYA
+    cd $BASE/ext/PyYAML-6.0                                                                                                  
+    python setup.py --without-libyaml install                                                                                
+    cd $BASE                                                                                                                 
+  else  
+    # A device with larger memory like a Cerbo can use Pip as normal                                                                                                                     
+    echo "dbus-mqtt-devices: Pip install module dependencies"                                                                
+    python -m pip install -r requirements.txt                                                                                
+  fi                                                                                                                         
+else                                                                                                                         
+  echo "dbus-mqtt-devices: Yaml is already available, no need to install PyYaml"                                             
+fi     
 
 echo "dbus-mqtt-devices: Set up Victron module libraries"
 rm -fr $BASE/ext/dbus-mqtt $BASE/ext/velib_python
