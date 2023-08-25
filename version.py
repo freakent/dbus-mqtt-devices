4604b9e3fe3a0c65e250a0acc5a75e2ae9ccb9d2
= <=>
2023-08-25 18:30:38 +0100

    Prepare beta with new config files

--- version.py
+++ version.py
@@ -6,7 +6,7 @@ def VERSION():
     try:                                                    
         base = os.path.dirname(os.path.realpath(__file__))  
         with open(os.path.join(base, 'VERSION'), 'r') as version_file:
-            version = version_file.read()
+            version = version_file.read().strip()
             return version
     except IOError as e:                                                                            
         logging.error("I/O error(%s): %s", e.errno, e.strerror)
